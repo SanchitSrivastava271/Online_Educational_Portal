ea4969dad8c68fc72c8b60010e8ec10e3fca8d63
sanch <211b271@juetguna.in>
2024-05-01 08:10:59 +0530

    modified banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,7 +47,7 @@ const Home = () => {
         </div>
 
         <div className="mx-3 my-7 shadow-[10px_-5px_50px_-5px] shadow-blue-200">
-            <video muted loop autoPlay className = "shadow-[20px_20px_rgba(255,255,255)]"> <source src = {Banner} type = "video/mp4"/> </video>
+            <video muted loop autoPlay className = ""> <source src = {Banner} type = "video/mp4"/> </video>
         </div>
 
         <div>
